120a3c0a55c6cf5bc34018abd8ce0f72f91bf4bc
binhsonnguyen <son@binhsonnguyen.com>
2018-02-01 22:32:04 +0700

    fix use of express static

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let express = require('express')
 let app = express()
 
-app.use(express.static('/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
-app.get('/', function(req, res) {
+
+app.get('/', (req, res) => {
   res.render('greeting')
 })
 
-app.listen(8000, function() {
+app.listen(8000, () => {
   console.log('Server started')
 })
