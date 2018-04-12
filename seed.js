3770220a2134d0806fb1dd20e880771c6887b93c
Your Name <you@example.com>
2018-04-12 22:59:26 +0700

    Update seed.js

--- seed.js
+++ seed.js
@@ -3,16 +3,19 @@
 const mongoose = require('mongoose')
 let Evac = require('./models/evac')
 
-mongoose.connect('mongodb://binhsonnguyen.com:8000/webmap-dev')
+mongoose.connect('mongodb://binhsonnguyen.com:8000/webmap-production')
 
 
 //TODO: Hỏi thầy về ASYNC JAVASCRIPT để export streamfile() ra một array thống nhất
-function streamfile(filename) {
+function streamVehicleEvacFile(filePath) {
   const StreamArray = require('stream-json/utils/StreamArray');
   const path = require('path');
   const fs = require('fs');
   let jsonStream = StreamArray.make();
-  let output = []
+  // let output = []
+  Evac.remove({}, function(err, result) {
+    if (err) return console.log('Cannot remove all db in collection Evac: ' + err)
+  })
 //You'll get json objects here
   jsonStream.output.on('data', function ({index, value}) {
     let newEvac = new Evac({
@@ -27,7 +30,7 @@ function streamfile(filename) {
         return {x: value[1], y: value[0]}
       })
     })
-    output.push(newEvac)
+    // output.push(newEvac)
     newEvac.save(function(err, result) {
       if (err) console.log(err)
     })
@@ -36,8 +39,45 @@ function streamfile(filename) {
     console.log('YES!')
   });
   let filepath = path.join(__dirname, filename);
-  fs.createReadStream(filepath).pipe(jsonStream.input);
-  return output
+  fs.createReadStream(filePath).pipe(jsonStream.input);
+  // return output
 }
-let output = streamfile('')
-console.log(output[0])
+
+function streamWalkEvacFile(filePath) {
+  const StreamArray = require('stream-json/utils/StreamArray');
+  const path = require('path');
+  const fs = require('fs');
+  let jsonStream = StreamArray.make();
+  // let output = []
+  Evac.remove({}, function(err, result) {
+    if (err) return console.log('Cannot remove all db in collection Evac: ' + err)
+  })
+//You'll get json objects here
+  jsonStream.output.on('data', function ({index, value}) {
+    let newEvac = new Evac({
+      forAddress: value['properties']['FULLADD'],
+      addressGPS: {
+        x: value['geometry']['coordinates'][0][1],
+        y: value['geometry']['coordinates'][0][0]
+      },
+      length: value['properties']['LENGTH_GEO'],
+      driveTimeEstimated: value['properties']['Minute'],
+      drive: value['geometry']['coordinates'].map(value => {
+        return {x: value[1], y: value[0]}
+      })
+    })
+    // output.push(newEvac)
+    newEvac.save(function(err, result) {
+      if (err) console.log(err)
+    })
+  });
+  jsonStream.output.on('end', function () {
+    console.log('YES!')
+  });
+  let filepath = path.join(__dirname, filename);
+  fs.createReadStream(filePath).pipe(jsonStream.input);
+  // return output
+}
+
+streamVehicleEvacFile('E:/coding/Javascript/Projects/webmap-project-json/AddVeAll.geojson')
+
