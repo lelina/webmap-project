21249d6bb88f06f4ae2690dcf64bc1b9e04d04df
binhsonnguyen <son@binhsonnguyen.com>
2018-04-15 12:19:47 +0700

    route for finding an evac

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ mongoose.connect(`${process.env.MONGO}/webmap-production`)
 
 const bodyParser = require('body-parser')
 
+const DriveEvac = require('./models/drive_evac')
+const WalkEvac = require('./models/walk_evac')
+
+const DEBUG = process.env.DEBUG
+
 let app = express()
 app.set('views', './views')
 app.set('view engine', 'ejs')
@@ -15,17 +20,35 @@ app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(use404)
+// app.use(use404)
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/evac/:lat/:lon', (req, res) => {
-  let coor = getEvac(req.params['evacId'])
-  res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify(coor))
+  let coor = {
+    lat: req.params.lat,
+    lon: req.params.lon
+  }
+
+  findEvac(coor, onErr, onFound)
+
+  function onErr (err) {
+    log(err)
+  }
+
+  function onFound (evac) {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(evac))
+  }
 })
+
+function findEvac(coor, onErr, onFound) {
+  DriveEvac.findOne({}, function (err, evac) {
+    return !!err ? onErr(err) : onFound(evac)
+  })
+}
 
 // 404 ROUTE
 
@@ -33,10 +56,10 @@ app.listen(8000, () => {
   console.log('Server started at http://localhost:8000')
 })
 
-function getEvac (evacId) {
-  return getFaker()
-}
-
 function use404 (req, res, next) {
   res.status(404).render('404.ejs')
+}
+
+function log (msg) {
+  if (DEBUG) console.log(`APP: ${msg}`)
 }
