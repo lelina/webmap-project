ac1e5ab59d9889a6a32621944e5796e167f55c43
binhsonnguyen <son@binhsonnguyen.com>
2018-05-18 10:22:12 +0700

    addresses API

--- app.js
+++ app.js
@@ -14,26 +14,45 @@ const WalkEvac = require('./models/walk_evac')
 const DEBUG = process.env.DEBUG
 
 let app = express()
+
+let walkAddresses = []
+let driveAddresses = []
+let allAddresses = []
+
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-// app.use(use404)
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.get('/addresses', (req, res) => {
-  let addresses = [
-    {id: 1, address: '44 taylor'},
-    {id: 2, address: '42 taylor'},
-    {id: 3, address: '41 taylor'}
-  ]
+  if (!allAddresses || allAddresses.length == 0) {
+    let id = 0
+    allAddresses = walkAddresses.map(walk => ({
+      id: id++,
+      readId: {walk: walk.id},
+      address: walk.address
+    }))
+    driveAddresses.forEach(drive => {
+      let previousAdded = allAddresses.find(existed => existed.address === drive.address)
+      if (!!previousAdded) {
+        previousAdded.readId.drive = drive.id
+      } else {
+        allAddresses.push({
+          id: id++,
+          readId: {drive: drive.id},
+          address: drive.address
+        })
+      }
+    })
+  }
   res.setHeader('Content-Type', 'application/json')
-  res.send(JSON.stringify(addresses))
+  res.send(JSON.stringify(allAddresses))
 })
 
 app.get('/drive/:suggestion', (req, res) => {
@@ -52,7 +71,7 @@ app.get('/drive/:suggestion', (req, res) => {
   }
 
   function findDriveEvac (address, onErr, onFound) {
-    let regex = new RegExp('^'+address+'$', "i")
+    let regex = new RegExp('^' + address + '$', 'i')
     DriveEvac.findOne({forAddress: address}, function (err, evac) {
       return !!err || !evac ? onErr(err) : onFound(evac)
     })
@@ -97,15 +116,23 @@ app.post('/walk/:lat/:lon', (req, res) => {
 
 })
 
-// 404 ROUTE
+WalkEvac.find({}).exec((err, evacs) => {
+  evacs.forEach(function (evac) {
+    walkAddresses.push({id: evac._id, address: evac.forAddress})
+    log('walkEvacs ' + walkAddresses.length)
+  })
+})
+
+DriveEvac.find({}).exec((err, evacs) => {
+  evacs.forEach(function (evac) {
+    driveAddresses.push({id: evac._id, address: evac.forAddress})
+    log('driveEvacs ' + driveAddresses.length)
+  })
+})
 
 app.listen(8000, () => {
   console.log('Server started at http://localhost:8000')
 })
-
-function use404 (req, res, next) {
-  res.status(404).render('404.ejs')
-}
 
 function log (msg) {
   if (DEBUG) console.log(`APP: ${msg}`)
